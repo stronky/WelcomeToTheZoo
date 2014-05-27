e49504357b18fb1863a03c2fb82cc98b1486c776
stronky <stronky@yahoo.com>
2014-05-27 22:13:44 +0200

    Test with playing audio: Media plugin
    Test with playing audio: Media plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 	$("img").click(function() {
+		var sound1 = new Media("sounds/sound1.mp3");
+		sound1.play();
+
 		//$(this).next("audio").get(0).play();
 	});
 });
+
+function playAudio() {
+	var myMedia = new Media("sounds/sound1.mp3");
+	myMedia.play();	
+}
+
+function playAudio2(url) {
+    // Play the audio file at url
+    var my_media = new Media(url,
+        // success callback
+        function() {
+            console.log("playAudio():Audio Success");
+            alert("playAudio():Audio Success");
+        },
+        // error callback
+        function(err) {
+            console.log("playAudio():Audio Error: "+err);
+            alert("playAudio():Audio Error"+err);
+    });
+
+    // Play audio
+    my_media.play();
+}
